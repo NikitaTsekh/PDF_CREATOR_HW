c5c5dd2fa79fa78024607d847b8c72587ac528df
nceh1988 <nceh1988@gmail.com>
2023-04-25 16:36:44 +0300

    commit2

--- pdf_creator_app/views.py
+++ pdf_creator_app/views.py
@@ -1,25 +1,25 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from .models import ReplenismentEvent
-import json
-# Create your views here.
-@csrf_exempt
-def endpoint1(request):
-    if request.method == 'POST':
-        data = json.loads(request.body)
-        my_model = ReplenismentEvent.objects.create(
-            name=data['name'],
-            age=data['age'],
-        )
-        response_data = {
-            'status': 'success',
-            'data': {
-                'id': my_model.id,
-                'name': my_model.name,
-                'age': my_model.age,
-            }
-        }
-        return JsonResponse(response_data)
-    else:
-        return JsonResponse({'status': 'error', 'message': 'Invalid request method.'})
+# from django.shortcuts import render
+# from django.http import JsonResponse
+# from django.views.decorators.csrf import csrf_exempt
+# from .models import ReplenismentEvent
+# import json
+# # Create your views here.
+# @csrf_exempt
+# def endpoint1(request):
+#     if request.method == 'POST':
+#         data = json.loads(request.body)
+#         my_model = ReplenismentEvent.objects.create(
+#             name=data['name'],
+#             age=data['age'],
+#         )
+#         response_data = {
+#             'status': 'success',
+#             'data': {
+#                 'id': my_model.id,
+#                 'name': my_model.name,
+#                 'age': my_model.age,
+#             }
+#         }
+#         return JsonResponse(response_data)
+#     else:
+#         return JsonResponse({'status': 'error', 'message': 'Invalid request method.'})
