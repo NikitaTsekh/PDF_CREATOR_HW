1e5c9efd5f9b5ee2dce3e26621377875c368e633
nceh1988 <nceh1988@gmail.com>
2023-04-25 18:07:07 +0300

    commit3

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 from api.views import CompanyModelViewSet
+from pdf_creator_app.views import endpoint1
 
 app_name = 'api'
 
@@ -9,6 +10,7 @@ router.register(r'companies',CompanyModelViewSet)
 
 urlpatterns = [
    path('', include(router.urls)),
+   path('organization-name', endpoint1),
    # path('', ProductListAPIView.as_view(), name='product_list'),
 ]
 
--- pdf_creator_app/views.py
+++ pdf_creator_app/views.py
@@ -1,25 +1,34 @@
-# from django.shortcuts import render
-# from django.http import JsonResponse
-# from django.views.decorators.csrf import csrf_exempt
-# from .models import ReplenismentEvent
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import ReplenismentEvent
 # import json
 # # Create your views here.
 # @csrf_exempt
-# def endpoint1(request):
-#     if request.method == 'POST':
-#         data = json.loads(request.body)
-#         my_model = ReplenismentEvent.objects.create(
-#             name=data['name'],
-#             age=data['age'],
-#         )
-#         response_data = {
-#             'status': 'success',
-#             'data': {
-#                 'id': my_model.id,
-#                 'name': my_model.name,
-#                 'age': my_model.age,
-#             }
-#         }
-#         return JsonResponse(response_data)
-#     else:
-#         return JsonResponse({'status': 'error', 'message': 'Invalid request method.'})
+@api_view(['POST'])
+def endpoint1(request):
+    if request.method == 'POST':
+        replenishment_id = request.data.get("replenishment_id")
+        replenishment_event = ReplenismentEvent.objects.get(pk=replenishment_id)
+
+        return Response(data=replenishment_event.organization_name)
+
+
+        # data = json.loads(request.body)
+    #     my_model = ReplenismentEvent.objects.create(
+    #         name=data['name'],
+    #         age=data['age'],
+    #     )
+    #     response_data = {
+    #         'status': 'success',
+    #         'data': {
+    #             'id': my_model.id,
+    #             'name': my_model.name,
+    #             'age': my_model.age,
+    #         }
+    #     }
+    #     return JsonResponse(response_data)
+    # else:
+    #     return JsonResponse({'status': 'error', 'message': 'Invalid request method.'})
