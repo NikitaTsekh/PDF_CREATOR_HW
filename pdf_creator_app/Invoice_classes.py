bca98978ce14ee857ce7b92737abfb0a27cafa79
nceh1988 <nceh1988@gmail.com>
2023-04-26 11:28:27 +0300

    commit4 "Sucess PDF but file"

--- pdf_creator_app/Invoice_classes.py
+++ pdf_creator_app/Invoice_classes.py
@@ -1,10 +1,13 @@
-import jinja2
+
 import pdfkit
 from dataclasses import dataclass
 from decimal import Decimal
 import io
 from num2words import num2words
 from datetime import datetime
+from django.template import loader
+from django.http import HttpResponse
+from django.conf import settings
 
 
 @dataclass()
@@ -62,41 +65,42 @@ class InvoiceCreatorService:
 
         self.result_str = f"{int_part_words} {ruble_word} {dec_part_words}"
 
+        # return self.result_str это затычка для теста
+
         context = {'invoice_number': self.invoice_number, 'invoice_date': self.invoice_date,
                    'organization_name': self.replenishment_event.organization_name,
-                   'organization_legal_address': self.replenishment_event.organization_legal_address,
-                   'organization_inn': self.replenishment_event.organization_inn,
-                   'organization_kpp': self.replenishment_event.organization_kpp,
-                   'contract_number': self.replenishment_event.contract_number,
-                   'contract_date': self.replenishment_event.contract_date,
-                   'payment_sum_without_commission': self.replenishment_event.payment_sum_without_commission,
-                   'commission_payment_sum': self.replenishment_event.commission_payment_sum,
-                   'total': self.replenishment_event.total, 'result_str': self.result_str}
+                    'organization_legal_address': self.replenishment_event.organization_legal_address,
+                    'organization_inn': self.replenishment_event.organization_inn,
+                     'organization_kpp': self.replenishment_event.organization_kpp,
+                     'contract_number': self.replenishment_event.contract_number,
+                    'contract_date': self.replenishment_event.contract_date,
+                    'payment_sum_without_commission': self.replenishment_event.payment_sum_without_commission,
+                    'commission_payment_sum': self.replenishment_event.commission_payment_sum,
+                    'total': self.replenishment_event.total, 'result_str': self.result_str}
 
-        template_loader = jinja2.FileSystemLoader('./')
-        template_env = jinja2.Environment(loader=template_loader)
-
-        template = template_env.get_template('invoice.html')
+        template = loader.get_template('invoice.html')
         output_text = template.render(context)
 
-        config = pdfkit.configuration(wkhtmltopdf='/usr/bin/wkhtmltopdf')
-        pdfkit.from_string(output_text, 'invoice.pdf', configuration=config)
-        #
-        pdf_bytes = pdfkit.from_string(output_text, output_path=False, configuration=config)
+        #template_loader = jinja2.FileSystemLoader('./')
+        # template_env = jinja2.Environment(loader=template_loader)
+        #template = template_env.get_template('invoice.html')  # output_text = template.render(context)
 
+        config = pdfkit.configuration(wkhtmltopdf='/usr/bin/wkhtmltopdf') #change at server
+        pdfkit.from_string(output_text, 'invoice.pdf', configuration=config)
+        pdf_bytes = pdfkit.from_string(output_text, output_path=False, configuration=config)
         pdf_io = io.BytesIO(pdf_bytes)
         return pdf_io
 
-
-if __name__ == '__main__':
-    megafon = ReplenismentEvent(organization_name='Мегафон',
-                                organization_legal_address='127006, г.москва , пер. Оружейный, д. 41',
-                                organization_inn='7812014560', organization_kpp='770701001', contract_number='1378А',
-                                contract_date='05.08.2021,', payment_sum_without_commission=2000.00,
-                                commission_payment_sum=200.00, total=2200.00)
-
-    invoice_service = InvoiceCreatorService(invoice_number="ЛК99", invoice_date='12.04.2026',
-                                            replenishment_event=megafon)
-
-    with open('output_invoice.pdf', 'wb') as f:
-        f.write(invoice_service.create_pdf().getvalue())
+#
+# if __name__ == '__main__':
+#     megafon = ReplenismentEvent(organization_name='Мегафон',
+#                                 organization_legal_address='127006, г.москва , пер. Оружейный, д. 41',
+#                                 organization_inn='7812014560', organization_kpp='770701001', contract_number='1378А',
+#                                 contract_date='05.08.2021,', payment_sum_without_commission=2000.00,
+#                                 commission_payment_sum=200.00, total=2200.00)
+#
+#     invoice_service = InvoiceCreatorService(invoice_number="ЛК99", invoice_date='12.04.2026',
+#                                             replenishment_event=megafon)
+#
+#     with open('output_invoice.pdf', 'wb') as f:
+#         f.write(invoice_service.create_pdf().getvalue())
--- pdf_creator_app/views.py
+++ pdf_creator_app/views.py
@@ -3,7 +3,9 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.http import HttpResponse
 from .models import ReplenismentEvent
+from .Invoice_classes import InvoiceCreatorService
 # import json
 # # Create your views here.
 # @csrf_exempt
@@ -13,8 +15,22 @@ def endpoint1(request):
         replenishment_id = request.data.get("replenishment_id")
         replenishment_event = ReplenismentEvent.objects.get(pk=replenishment_id)
 
-        return Response(data=replenishment_event.organization_name)
+        obj = InvoiceCreatorService(replenishment_event=replenishment_event,invoice_number='123',invoice_date='24.04.2023')
 
+        #return Response(data=obj.create_pdf())
+        #return HttpResponse(data=obj.create_pdf())
+        response = HttpResponse(obj.create_pdf(), content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename=invoice.pdf'
+        return response
+        #return Response(data=replenishment_event.organization_name)
+
+    # @api_view(['POST'])
+    # def endpoint1(request):
+    #     if request.method == 'POST':
+    #         replenishment_id = request.data.get("replenishment_id")
+    #         replenishment_event = ReplenismentEvent.objects.get(pk=replenishment_id)
+    #
+    #         return Response(data=replenishment_event.organization_name)
 
         # data = json.loads(request.body)
     #     my_model = ReplenismentEvent.objects.create(
